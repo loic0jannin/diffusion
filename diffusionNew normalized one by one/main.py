c759bfe97be7b280adf31808c20c6da20e82723f
Paul Faverjon <95045179+paulfav@users.noreply.github.com>
2024-04-18 18:36:55 -0700

    commit

--- diffusionNew normalized one by one/main.py
+++ diffusionNew normalized one by one/main.py
@@ -8,11 +8,17 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data import TensorDataset
 
+
+print("Importing the data...")
+
 # import the normalized slices
 slices = pd.read_csv('data/slices_normalized.csv', index_col=0)
 
+print("Data imported successfully!")
+print("Converting the data to a PyTorch DataLoader...")
 # Convert the DataFrame to a numpy array, then to a PyTorch Tensor
 slices_tensor = torch.tensor(slices.values)
+print("Actuallly converting the data to a PyTorch DataLoader...")
 
 # Convert your data to a TensorDataset
 slices_dataset = TensorDataset(slices_tensor)
@@ -20,16 +26,23 @@ slices_dataset = TensorDataset(slices_tensor)
 # DEFINES THE BATCH SIZE
 batch_size = 32
 
+print("slices_loader")
+
 # Create a DataLoader with shuffle=True for shuffling at each epoch
 slices_loader = DataLoader(slices_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
 
+print("DataLoader created successfully!")
 
 # import the backward file and functions
 import backward
 from torch.optim import Adam
 
+print("creating the model...")
+
 # Create an instance of the denoising network
 model = backward.DenoisingNetwork()
+
+print("Model created successfully!")
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
